960bb1c0cd199d3d5c7d6e019c11e927659961da
bluesliverx <bksaville@gmail.com>
2012-02-06 16:56:56 -0700

    Removed conflicted file states

--- grails-app/controllers/test/LogoutController.groovy
+++ grails-app/controllers/test/LogoutController.groovy
@@ -1,5 +1,4 @@
-package test
-<<<<<<< HEAD
+start package test
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 
 class LogoutController {
@@ -12,17 +11,3 @@ class LogoutController {
 		redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
 	}
 }
-=======
-import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
-
-class LogoutController {
-
-	/**
-	 * Index action. Redirects to the Spring security logout uri.
-	 */
-	def index = {
-		// TODO put any pre-logout code here
-		redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
-	}
-}
->>>>>>> aaccc0d5cdb6c3fef4e58fe1a0c5a7c76879cc4a
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 <html>
 <head><title>OAuth2 Provider Test Page</title></head>
 <body>
 	<p>User: <sec:ifLoggedIn><sec:username /></sec:ifLoggedIn><sec:ifNotLoggedIn>Not Logged In</sec:ifNotLoggedIn></p>
 </body>
-=======
-<html>
-<head><title>OAuth2 Provider Test Page</title></head>
-<body>
-	<p>User: <sec:ifLoggedIn><sec:username /></sec:ifLoggedIn><sec:ifNotLoggedIn>Not Logged In</sec:ifNotLoggedIn></p>
-</body>
->>>>>>> aaccc0d5cdb6c3fef4e58fe1a0c5a7c76879cc4a
 </html>
