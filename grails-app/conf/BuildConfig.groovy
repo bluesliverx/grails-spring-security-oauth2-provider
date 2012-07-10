538754394a4244713d5da121d35634059d020fd5
Brian Saville <bsaville@adaptivecomputing.com>
2012-07-10 08:12:50 -0600

    Move workdir to target dir

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,4 @@
+grails.work.dir = "target/workdir"
 grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir	= 'target/test-reports'
