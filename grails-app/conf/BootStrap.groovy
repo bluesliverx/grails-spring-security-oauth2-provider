49f05cde61731700b7086b5117cc8bd580f345c2
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-03-16 17:56:08 -0400

    Clean up BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,16 +1,5 @@
-import grails.util.Environment
-import test.*
 
 class BootStrap {
 	def init = { servletContext ->
-		if (Environment.isDevelopmentMode()) {
-			// Add a test user
-			User user = new User(
-				username:"admin",
-				password:"password"
-			).save(failOnError:true)
-			Role role = new Role(authority:"ROLE_ADMIN").save(failOnError:true)
-			UserRole.create user, role, true
-		}
 	}
 }
