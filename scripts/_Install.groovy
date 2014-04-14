17ab8be345f539aeb64c20b01febba69b455cf60
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-04-13 22:34:48 -0400

    Add install message

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,3 +1,14 @@
 ant.mkdir dir: "$basedir/grails-app/views/oauth"
 ant.copy(file:"$pluginBasedir/grails-app/views/oauth/confirm_access.gsp",
          todir:"$basedir/grails-app/views/oauth", failonerror:false, overwrite:false)
+
+println '''
+*******************************************************
+* You've installed the Spring Security OAuth 2.0      *
+* Provider plugin.                                    *
+*                                                     *
+* Next run the "s2-init-oauth2-provider" script to    *
+* create your domain classes.                         *
+*                                                     *
+*******************************************************
+'''
