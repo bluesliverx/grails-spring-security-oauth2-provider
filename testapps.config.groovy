144d780afca65e0ec24fa8ddeacc3791c36f48af
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-05-23 21:58:55 -0400

    Add create-oauth2-test-apps script and config

---
+++ testapps.config.groovy
@@ -0,0 +1,103 @@
+v20 {
+	grailsVersion = '2.0.4'
+	pluginVersion = version
+	dotGrails = dotGrailsCommon
+	projectDir = projectDirCommon
+	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v21 {
+	grailsVersion = '2.1.4' // 2.1.5 has a plugin i18n bug
+	pluginVersion = version
+	dotGrails = dotGrailsCommon
+	projectDir = projectDirCommon
+	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v22 {
+	grailsVersion = '2.2.4'
+	pluginVersion = version
+	dotGrails = dotGrailsCommon
+	projectDir = projectDirCommon
+	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v230 {
+    grailsVersion = '2.3.0'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v231 {
+    grailsVersion = '2.3.1'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v232 {
+    grailsVersion = '2.3.2'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v233 {
+	grailsVersion = '2.3.3'
+	pluginVersion = version
+	dotGrails = dotGrailsCommon
+	projectDir = projectDirCommon
+	grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v234 {
+    grailsVersion = '2.3.4'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v235 {
+    grailsVersion = '2.3.5'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v236 {
+    grailsVersion = '2.3.6'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v237 {
+    grailsVersion = '2.3.7'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v238 {
+    grailsVersion = '2.3.8'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
+
+v240 {
+    grailsVersion = '2.4.0'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/grails-' + grailsVersion
+}
