18f60d3c8a8a8f8370391e1dfe7b7644cc07c74d
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-05-23 21:57:13 -0400

    Copy simple error page install time

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,6 +1,10 @@
 ant.mkdir dir: "$basedir/grails-app/views/oauth"
+
 ant.copy(file:"$pluginBasedir/grails-app/views/oauth/confirm_access.gsp",
          todir:"$basedir/grails-app/views/oauth", failonerror:false, overwrite:false)
+
+ant.copy(file:"$pluginBasedir/grails-app/views/oauth/error.gsp",
+        todir:"$basedir/grails-app/views/oauth", failonerror:false, overwrite:false)
 
 println '''
 *******************************************************
