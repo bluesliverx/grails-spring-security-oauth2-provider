e5f048779d27c932e57dd0829b044532d7064505
Brian Saville <saville@adobe.com>
2015-03-05 20:02:40 -0700

    Only publish to grails if it's a versioned branch and don't run functional tests for now

--- travis-build.sh
+++ travis-build.sh
@@ -2,10 +2,12 @@
 # Credit to candrews for his example of a travis ci build for a grails plugin
 set -e
 ./grailsw refresh-dependencies --non-interactive --stacktrace
-./grailsw test-app --non-interactive --stacktrace
+#./grailsw test-app --non-interactive --stacktrace
+./grailsw test-app unit: integration: --non-interactive --stacktrace
 ./grailsw package-plugin --non-interactive --stacktrace
 ./grailsw doc --pdf --non-interactive --stacktrace
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spring-security-oauth2-provider' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+# Only publish if the branch starts with a number (i.e. 1.0.0 but not master)
+if [[ $TRAVIS_BRANCH =~ ^[0-9].*$ && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spring-security-oauth2-provider' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   git config --global user.name "$GIT_NAME"
   git config --global user.email "$GIT_EMAIL"
   git config --global credential.helper "store --file=~/.git-credentials"
@@ -21,5 +23,5 @@ if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spr
   rm -rf gh-pages
   ./grailsw publish-plugin --no-scm --allow-overwrite --non-interactive --stacktrace
 else
-  echo "Not on master branch, so not publishing"
+  echo "Not on versioned branch, so not publishing"
 fi
