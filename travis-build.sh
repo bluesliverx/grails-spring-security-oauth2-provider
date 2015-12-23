159481057f6355e32b6159161f258679dee7db9e
Brian Saville <bsaville@adaptivecomputing.com>
2015-12-23 13:19:20 -0700

    Take out doc publishing for now

--- travis-build.sh
+++ travis-build.sh
@@ -9,23 +9,23 @@ rm -rf *.zip
 if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
   echo "Publishing plugin grails-spring-security-oauth2-provider"
 
-  if [[ $filename != *-SNAPSHOT* && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spring-security-oauth2-provider' ]]; then
-    git config --global user.name "$GIT_NAME"
-    git config --global user.email "$GIT_EMAIL"
-    git config --global credential.helper "store --file=~/.git-credentials"
-    echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
-    git clone https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
-    cd gh-pages
-    git rm -rf .
-    cp -r ../spring-security-oauth2-provider/build/docs/. ./
-    git add *
-    git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-    git push origin HEAD
-    cd ..
-    rm -rf gh-pages
-  else
-    echo "Not doing a release, so not publishing docs"
-  fi
+  #if [[ $filename != *-SNAPSHOT* && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spring-security-oauth2-provider' ]]; then
+  #  git config --global user.name "$GIT_NAME"
+  #  git config --global user.email "$GIT_EMAIL"
+  #  git config --global credential.helper "store --file=~/.git-credentials"
+  #  echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
+  #  git clone https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
+  #  cd gh-pages
+  #  git rm -rf .
+  #  cp -r ../spring-security-oauth2-provider/build/docs/. ./
+  #  git add *
+  #  git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+  #  git push origin HEAD
+  #  cd ..
+  #  rm -rf gh-pages
+  #else
+  #  echo "Not doing a release, so not publishing docs"
+  #fi
 
   # Publish plugin
   ./gradlew spring-security-oauth2-provider:bintrayUpload --stacktrace
