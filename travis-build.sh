0cf9a58b3d1bfc92cb4068e09606bbfda35d2c5c
Brian Saville <bsaville@adaptivecomputing.com>
2016-01-06 20:47:36 -0700

    Attempt to get 3.x build working again

--- travis-build.sh
+++ travis-build.sh
@@ -6,28 +6,38 @@ rm -rf *.zip
 #./gradlew acceptanceTest -Dhttp.basic=true --stacktrace
 ./gradlew spring-security-oauth2-provider:gdocs --stacktrace
 
+cd spring-security-oauth2-provider/build/libs
+filename=$(find . -name "*.jar" | grep -v javadoc | grep -v sources | head -1)
+filename=$(basename $filename)
+plugin=${filename:7}
+plugin=${plugin/.jar/}
+plugin=${plugin/-SNAPSHOT/}
+version="${plugin#*-}";
+plugin=${plugin/"-$version"/}
+cd ../../..
+
 if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
   echo "Publishing plugin grails-spring-security-oauth2-provider"
 
-  #if [[ $filename != *-SNAPSHOT* && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spring-security-oauth2-provider' ]]; then
-  #  git config --global user.name "$GIT_NAME"
-  #  git config --global user.email "$GIT_EMAIL"
-  #  git config --global credential.helper "store --file=~/.git-credentials"
-  #  echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
-  #  git clone https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
-  #  cd gh-pages/v3
-  #  git rm -rf .
-  #  cp -r ../../spring-security-oauth2-provider/build/docs/. ./
-  #  git add *
-  #  git commit -a -m "Updating 3.x docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-  #  git push origin HEAD
-  #  cd ../..
-  #  rm -rf gh-pages
-  #else
-  #  echo "Not doing a release, so not publishing docs"
-  #fi
+  if [[ $filename != *-SNAPSHOT* && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spring-security-oauth2-provider' ]]; then
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global credential.helper "store --file=~/.git-credentials"
+    echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
+    git clone https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
+    cd gh-pages/v3
+    git rm -rf .
+    cp -r ../../spring-security-oauth2-provider/build/docs/. ./
+    git add *
+    git commit -a -m "Updating 3.x docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+    git push origin HEAD
+    cd ../..
+    rm -rf gh-pages
 
-  # Publish plugin
-  ./gradlew spring-security-oauth2-provider:bintrayUpload --stacktrace
+    # Publish plugin
+    ./gradlew spring-security-oauth2-provider:bintrayUpload --stacktrace
+  else
+    echo "Not doing a release, so not publishing"
+  fi
 
 fi
