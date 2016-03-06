540f1485c663e74ee569867b1e6920d147e7145f
Bobby Vandiver <bobby.vandiver88@gmail.com>
2016-03-06 00:34:01 -0500

    Disable gh-pages just to cut the final 3.0.0-RC1 artifact

--- travis-build.sh
+++ travis-build.sh
@@ -18,20 +18,20 @@ if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
   echo "Publishing plugin grails-spring-security-oauth2-provider with version $version"
 
   if [[ $filename != *-SNAPSHOT* && $TRAVIS_REPO_SLUG == 'bluesliverx/grails-spring-security-oauth2-provider' ]]; then
-    git config --global user.name "$GIT_NAME"
-    git config --global user.email "$GIT_EMAIL"
-    git config --global credential.helper "store --file=~/.git-credentials"
-    echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
-
-    git clone https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
-    cd gh-pages
-    git rm -rf v3/*
-    cp -rp ../spring-security-oauth2-provider/build/docs/. v3/
-    git add *
-    git commit -a -m "Updating 3.x docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-    git push origin HEAD
-    cd ..
-    rm -rf gh-pages
+#    git config --global user.name "$GIT_NAME"
+#    git config --global user.email "$GIT_EMAIL"
+#    git config --global credential.helper "store --file=~/.git-credentials"
+#    echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
+#
+#    git clone https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
+#    cd gh-pages
+#    git rm -rf v3/*
+#    cp -rp ../spring-security-oauth2-provider/build/docs/. v3/
+#    git add *
+#    git commit -a -m "Updating 3.x docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+#    git push origin HEAD
+#    cd ..
+#    rm -rf gh-pages
 
     # Publish plugin
     ./gradlew spring-security-oauth2-provider:bintrayUpload --stacktrace
